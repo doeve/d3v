61a99dacd61492721767c7793a06783f13ee6a0e
dave <serb_david@yahoo.com>
2025-04-08 23:37:07 +0300

    updated frontend dependencies

---
+++ frontend/src/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import { ToastProvider } from './components/common/Toast';
+
+// Apply Tailwind styles
+import './tailwind.css';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ToastProvider>
+        <App />
+      </ToastProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
